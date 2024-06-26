d83339f85fd6a7534b8a7ca2b27540c19926107d
BANER <86590991+BANSAFAn@users.noreply.github.com>
2024-06-26 13:57:06 +0300

    Roles Id & Names Roles

---
+++ id&nameroles.rs
@@ -0,0 +1,52 @@
+nmroles: ("[HZ]") {
+    id: <1052315586165756015>
+}
+
+nmroles: ("A man who would sell me out for chips.") {
+    id: <988359380858699776>
+}
+
+
+nmroles: ("⌊🤖⌉ Bot") {
+    id: <838690277376000002>
+}
+
+
+nmroles: ("TA") {
+    id: <894891903462559775>
+}
+
+
+nmroles: ("🧨admin hand") {
+    id: <835553128354938910>
+}
+
+
+nmroles ("🔐defender") {
+    id: <1233854552130715748>
+}
+
+
+nmroles ("🔏surveillance") {
+    id: <1233854377895264346>
+}
+
+
+nmroles ("🔒 study....") {
+    id: <865920403117834240>
+}
+
+
+nmroles ("🤝") {
+    id: <1247169545404157952>
+}
+
+
+nmroles ("🔖Interpreters") {
+    id: <1255476217113477210>
+}
+
+
+// Idi and titles, for the translators
+
+
